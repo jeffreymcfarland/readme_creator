2f2e8847cc73e577310f51431684202924914cd4
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-04-05 13:28:34 -0500

    created and tested second set of inquirer prompts

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const axios = require("axios");
 
 var inquirer = require("inquirer");
 
-console.log("Welcome to the readme creator!");
+console.log("Welcome to the Readme Creator!");
+console.log("------------------------------------")
 
-var questions = [
+var question = [
     {
         type: "input",
         name: "username",
@@ -14,10 +15,8 @@ var questions = [
     }
 ];
 
-inquirer.prompt(questions).then(answer => {
+inquirer.prompt(question).then(answer => {
     const username = answer.username;
-
-    console.log(username);
 
     axios.get(`https://api.github.com/users/${username}`, {
     headers: {"Authorization": `token ${process.env.JM_TOKEN}`}
@@ -29,12 +28,48 @@ inquirer.prompt(questions).then(answer => {
 
     const avatar = response.data.avatar_url;
     const email = response.data.email;
+
+    nextPrompt();
+
     })
     .catch(function (error) {
     // handle error
     console.log(error);
-    })
+    });
+
 });
 
 
+var nextQuestions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is your github project title?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please give a description of your project."
+    },
+    {
+        type: "checkbox",
+        name: "Table of Contents",
+        message: "Select which of the following you want to include in the Table of Contents",
+        choices: [
+            "Installation",
+            "Usage",
+            "License",
+            "Contributing",
+            "Tests",
+            "Questions"
+        ]
+    }
+];
 
+function nextPrompt() {
+
+    inquirer.prompt(nextQuestions).then(answers => {
+        console.log(answers);
+    });
+
+};
