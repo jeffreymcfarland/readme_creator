f92ee470de654e5be8d5cf0ca2da126c9dcfac55
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-04-05 13:04:38 -0500

    successful call on github api to grab avatar_url and email of github user

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-require('dotenv').config()
+require("dotenv").config()
 
 const axios = require("axios");
 
-const username = "jeffreymcfarland";
+var inquirer = require("inquirer");
 
+console.log("Welcome to the readme creator!");
 
-axios.get(`https://api.github.com/users/${username}`, {
+var questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github username?"
+    }
+];
+
+inquirer.prompt(questions).then(answer => {
+    const username = answer.username;
+
+    console.log(username);
+
+    axios.get(`https://api.github.com/users/${username}`, {
     headers: {"Authorization": `token ${process.env.JM_TOKEN}`}
-})
-  .then(function (response) {
+    })
+    .then(function (response) {
     // handle success
-    console.log(response.data);
-  })
-  .catch(function (error) {
+    console.log(response.data.avatar_url);
+    console.log(response.data.email);
+
+    const avatar = response.data.avatar_url;
+    const email = response.data.email;
+    })
+    .catch(function (error) {
     // handle error
     console.log(error);
-  })
+    })
+});
+
+
+
