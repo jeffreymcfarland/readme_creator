70ac3cc5850197045afa7725f9b35c8d992b00c8
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-04-09 22:40:53 -0500

    successfully generated readme file

--- index.js
+++ index.js
@@ -2,47 +2,24 @@ require("dotenv").config()
 
 const axios = require("axios");
 
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+
+const fs = require("fs");
 
 inquirer.registerPrompt('emoji', require('inquirer-emoji'))
 
 console.log("Welcome to the Readme Creator!");
 console.log("------------------------------------")
 
-var question = [
+
+
+
+const nextQuestions = [
     {
         type: "input",
         name: "username",
         message: "What is your github username?"
-    }
-];
-
-inquirer.prompt(question).then(answer => {
-    const username = answer.username;
-
-    axios.get(`https://api.github.com/users/${username}`, {
-    headers: {"Authorization": `token ${process.env.JM_TOKEN}`}
-    })
-    .then(function (response) {
-    // handle success
-    console.log(response.data.avatar_url);
-    console.log(response.data.email);
-
-    const avatar = response.data.avatar_url;
-    const email = response.data.email;
-
-    nextPrompt();
-
-    })
-    .catch(function (error) {
-    // handle error
-    console.log(error);
-    });
-
-});
-
-
-const nextQuestions = [
+    },
     {
         type: "input",
         name: "Title",
@@ -65,8 +42,13 @@ const nextQuestions = [
     },
     {
         type: "input",
+        name: "usageCode",
+        message: "If applicable, please type what a user would need to input into the vs terminal to run this app."
+    },
+    {
+        type: "input",
         name: "Usage",
-        message: "Describe to the user how to use your project."
+        message: "Describe how to use your application."
     },
     {
         type: "list",
@@ -81,14 +63,103 @@ const nextQuestions = [
     }
 ];
 
-function nextPrompt() {
 
     inquirer.prompt(nextQuestions).then(answers => {
 
-        if (answers.License === "Other") {
-            console.log("-----Please update License section to include which license you decided to use.");
-        };
 
+        const username = answers.username;
+
+            axios.get(`https://api.github.com/users/${username}`, {
+            headers: {"Authorization": `token ${process.env.JM_TOKEN}`}
+            })
+            .then(function (response) {
+            // handle success
+
+            const avatar = response.data.avatar_url;
+            const email = response.data.email;
+
+            const run = "npm install";
+            if (answers.Installation !== true) {
+                console.log("-----Please update installation section to fit your project.")
+            };
+
+            if (answers.License === "Other") {
+                console.log("-----Please update License section to include name of which license you are using.");
+            };
+    
+            const title = answers.Title;
+            const about = answers.Description;
+            const emoji = answers.projectEmoji;
+    
+            const usageCode = answers.usageCode;
+            const usage = answers.Usage;
+    
+            const license = answers.License;
+            const contributing = answers.Contributing.split(",").join("\n\n");
+    
+            const markdown =
+`
+# <div align="center">${title}</div>
+
+## ${emoji} About
+
+${about}
+
+***
+## Table of Contents
+1. [Install](#install)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Contact Me](#contact-me)
+
+***
+## Install
+
+To install dependencies in this app, clone this repo and run 
+\`\`\`
+${run}
+\`\`\`
+
+***
+## Usage
+
+To use this app, simply run
+\`\`\`
+${usageCode}
+\`\`\`
+in the vs terminal. 
+${usage}
+
+***
+## License
+
+![badge](https://img.shields.io/badge/License-${license}-success)
+
+***
+## Contributing
+
+${contributing}
+
+***
+## Contact Me    
+
+<kbd><img src="${avatar}" /></kbd> 
+* Email: <${email}>
+`
+    
+            fs.writeFile("test.md", markdown, (err) => {
+                if (err) {
+                    return console.log(err);
+                };
+              });
+
+            })
+            .catch(function (error) {
+            // handle error
+            console.log(error);
+            });
+
+        
     });    
 
-};
